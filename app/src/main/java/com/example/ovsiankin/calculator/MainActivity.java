c80f0aa7cbfe9b4ee07aba03763c1155019c4674
Oleksandr Ovsiankin <ovsyankin.sasha@gmail.com>
2019-02-27 13:36:04 +0100

    add History

--- app/src/main/java/com/example/ovsiankin/calculator/MainActivity.java
+++ app/src/main/java/com/example/ovsiankin/calculator/MainActivity.java
@@ -96,7 +96,13 @@ public class MainActivity extends AppCompatActivity {
                 onEqual();
             }
         });
-
+        findViewById(R.id.btnHistory).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,History.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
